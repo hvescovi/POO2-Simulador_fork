8c90080c67626e4df576efb87624c2a4d0e95272
Matheus Marchi Moro <71672437+mathsmm@users.noreply.github.com>
2023-04-18 15:44:36 -0300

    SDL2 e arquivos diversos adicionados

---
+++ src/Particle.hpp
@@ -0,0 +1,32 @@
+#pragma once
+#include "Body.hpp"
+#include "Vect.hpp"
+#include <vector>
+
+class Particle : public Body
+/** Classe que representa uma part�cula circular. */
+{
+	private:
+		// Raio da part�cula (a part�cula � circular)
+		double radius;
+
+	public:
+		// Construtor padr�o.
+		Particle();
+
+		/**
+		 * Construtor. Inicializa uma part�cula.
+		 * 
+		 * \param initialPosition Vetor de posi��o inicial
+		 * \param initalVelocity Vetor de velocidade inicial
+		 * \param radius Raio
+		 */
+		Particle(Vect initialPosition, Vect initalVelocity, double radius);
+
+		double get_rad();
+
+		static std::vector<Particle> createVectorParticle1();
+		static std::vector<Particle> createVectorParticle2();
+		static std::vector<Particle> createVectorParticle3();
+		static std::vector<Particle> createVectorParticle4();
+};
---
+++ src/Vect.hpp
@@ -0,0 +1,86 @@
+#pragma once
+class Vect
+/** Vetor. Utiliza-se para representar e calcular grandezas vetoriais. */
+{
+	private:
+		// Coordenada x.
+		double x;
+
+		// Coordenada y.
+		double y;
+
+	public:
+		// Construtor vazio.
+		Vect();
+
+		/**
+		 * Construtor. Inicializa um vetor a partir de uma 
+		 * coordenada X e uma coordenada Y.
+		 *
+		 * \param coordX Coordenada X;
+		 * \param coordY Coordenada Y.
+		 */
+		Vect(double coordX, double coordY);
+
+		/**
+		 * Construtor. Inicializa um vetor a partir 
+		 * de outros dois vetores.
+		 *
+		 * \param firstCoords Vetor inicial
+		 * \param lastCoords Vetor final
+		 */
+		Vect(Vect first, Vect last);
+
+		Vect operator+ (const Vect& v) const
+		{
+			return Vect(x + v.x, y + v.y);
+		}
+
+		Vect operator- (const Vect& v)
+		{
+			return Vect(x - v.x, y - v.y);
+		}
+
+		double operator* (const Vect& v)
+		/** Produto escalar */
+		{
+			return x * v.x + y * v.y;
+		}
+
+		Vect operator* (const double& value)
+		{
+			return Vect(x * value, y * value);
+		}
+
+		Vect operator/ (const double& value)
+		{
+			return Vect(x / value, y / value);
+		}
+
+		double get_x();
+		double get_y();
+
+		// \return Vetor unit�rio
+		Vect unitVect();
+
+		/** \return M�dulo do vetor */
+		double Module();
+
+		/** \return Seno do argumento do vetor */
+		double VSin();
+
+		/** \return Cosseno do argumento do vetor */
+		double VCos();
+
+		/** \return Argumento do vetor */
+		double Argument();
+
+		/**
+		 * Altera as coordenadas do vetor de modo
+		 * a incrementar seu argumento.
+		 * 
+		 * \param radInc Incremento em radianos
+		 * \return void
+		 */
+		void IncArgument(double radInc);
+};
